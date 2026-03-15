c626e8ef294ff2f63ae5ca4615083f267757f9c4
Alejandro <alejandro.baena@udea.edu.co>
2026-03-15 16:10:25 -0500

    este commit añade las funciones de de movimiento en tres funciones move_right -> mover a la derecha, move_left -> mover a la izquierda, move_down -> mover hacia abajo

--- tetris/mov_piezas.cpp
+++ tetris/mov_piezas.cpp
@@ -132,3 +132,122 @@ unsigned char** Eliminar_fila(unsigned char** board,int rows,int cols){
 }
     return board;
 }
+
+bool move_right(unsigned char** board,int rows,int cols,int& x,int y,unsigned char* piece)
+{
+    int coor_x;
+
+    coor_x = x + 1; // la coordenada a mover a la derecha ->
+
+    for(int py = 0 ; py<4 ; py++ ){ // recorre la fila de la figura
+
+        unsigned char row = piece[py];
+
+        for(int px = 0 ; px < 8; px++){ // recorre los bits de la fila
+
+            int bit = (row >> (7 - px)) & 1;
+
+            if(bit){ // revisa si hay un bloque si hay debe ser bit = 1
+
+                int bx = coor_x + px, by = y + py ; //calcula la posicion del tablero
+
+                // revisa el limite de la pared derecha
+                if(bx >= cols * 8)
+                    return false;
+
+                // revisa el suelo
+                if(by >= rows)
+                    return false;
+
+                int byte = bx / 8; // calcular byte del tablero de la columna
+                int bitpos = 7 - (bx % 8); // mover el bit hasta la ultima posicion
+
+                if((board[by][byte] >> bitpos)& 1) // calcula si hay espacio disponible 1 = lleno, 0 = vacio
+                {
+                    return false;
+                }
+
+
+            }
+        }
+    }
+    x = coor_x ;
+    return true;
+}
+
+bool move_left(unsigned char** board,int rows,int cols,int& x,int y,unsigned char* piece)
+{
+    int coor_x;
+    coor_x = x - 1;
+
+    for(int py = 0 ; py<4 ; py++ ){ // recorre la fila de la figura
+
+        unsigned char row = piece[py];
+
+        for(int px = 0 ; px < 8; px++){ // recorre los bits de la fila
+
+            int bit = (row >> (7 - px)) & 1;
+
+            if(bit){// revisa si hay un bloque si hay debe ser bit = 1
+
+                int bx = coor_x + px, by = y + py ; //calcula la posicion del tablero
+
+                //revisar limite de la parde izquierda
+                if(bx < 0){
+
+                    return false;
+                }// limite del suelo
+                if(by >= rows){
+                    return false;
+                }
+                int byte = bx / 8; // calcular byte del tablero de la columna
+                int bitpos = 7 - (bx % 8); // mover el bit hasta la ultima posicion
+
+                if((board[by][byte] >> bitpos)& 1) // calcula si hay espacio disponible 1 = lleno, 0 = vacio
+                {
+                    return false;
+                }
+
+            }
+
+        }
+    }
+    x = coor_x ;
+    return true;
+}
+
+bool move_down(unsigned char** board,int rows,int cols,int x,int& y,unsigned char* piece)
+{
+    int coor_y;
+    coor_y = y + 1;
+
+    for(int py = 0; py < 4; py++)
+    {
+        unsigned char row = piece[py];
+
+        for(int px = 0; px < 8; px++)
+        {
+
+            int bit = (row >> (7 - px)) & 1;
+
+            if(bit)
+            {
+                int bx = x + px, by = coor_y + py ;
+
+                if(by >= rows){
+                    return false;
+                }
+                int byte = bx / 8; // calcular byte del tablero de la columna
+                int bitpos = 7 - (bx % 8); // mover el bit hasta la ultima posicion
+
+                if((board[by][byte] >> bitpos)& 1) // calcula si hay espacio disponible 1 = lleno, 0 = vacio
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+    y = coor_y;
+    return true;
+}
